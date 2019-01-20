206c0a9ecdb981c1d5c0a111fbdbd92f00c3357b
udjindal <jindaludit23@gmail.com>
2019-01-20 20:36:13 +0530

    Added Subtraction.

--- caladd.cpp
+++ caladd.cpp
@@ -10,7 +10,8 @@ public class caladd
 		do
 		{
 			System.out.println("1. Add 2 Numbers \n");
-			System.out.println("2. Exit \n\n");
+            System.out.println("2. Subtract 2 Numbers \n");
+			System.out.println("3. Exit \n\n");
 			System.out.println("Enter Your Choice: ");
 			choice = scan.next().charAt(0);
 			switch(choice)
@@ -22,7 +23,13 @@ public class caladd
 							System.out.println("Result = " + res);
 							break;
 
-				case '2' :	System.exit(0);
+                case '2' :	System.out.println("Enter 2 Numbers: ");
+							a = scan.nextFloat();
+							b = scan.nextFloat();
+							res = a - b;
+							System.out.println("Result = " + res);
+							break;
+				case '3' :	System.exit(0);
 							break;
 
 				default  :	System.out.println("Invalid Choice!!!");
@@ -30,6 +37,6 @@ public class caladd
 			}
 			System.out.println("\n ------------------------- \n");
 
-		}while(choice != 2);
+		}while(choice != 3);
 	}
 }
